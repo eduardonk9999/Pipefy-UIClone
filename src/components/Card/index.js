7fe7843c999d68cbb68d386af9a023c6caad4951
eduardonk9999 <thiago.novak7@gmail.com>
2020-11-22 20:41:17 -0300

    Add, finalizando front

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { Container, Label } from './styles';
 
-export default function Card() {
+export default function Card({ data }) {
   return (
     <Container>
       <header>
-        <Label color="#7159c1" />
+        {data.labels.map(label =>  <Label color={label} /> )}
+       
       </header>
-      <p>Fazer a migração completa de servidor</p>
+      <p>{data.content}</p>
       <img src="https://avatars2.githubusercontent.com/u/18013936?s=460&u=5a39ab5d8335e38836f55a65aca512eb296a86e5&v=4" alt="Avatar"/>
     </Container>
   );
